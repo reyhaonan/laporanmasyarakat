a6428749458ea715d859ea9e6cde1075e930cf02
Kauva <Kauva@users.noreply.github.com>
2021-03-27 16:20:33 +0700

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -8,44 +8,65 @@
 
 
         <style>
+        @import url('https://fonts.googleapis.com/css2?family=Ubuntu&display=swap');
             body {
-                font-family: 'Poppins', sans-serif;
+                background-color: #313040;
+                overflow: hidden;
             }
             .adminbutton {
+                margin-left: 480px;
+            }
+            .civilbutton {
+                margin-left: 50px;
+            }
+            .text {
+                font-family: 'Ubuntu', sans-serif;
+                text-align: center;
+                margin-top: 250px;
+                font-weight: 650;
+                color: white;
+            }
+            .square {
+                position: absolute;
+                left: -100px;
+                bottom: -125px;
+                height: 400px;
+                width: 400px;
+                border: 8px solid #555;
+                border-radius: 65px;
+                fill: none;
+                border-color: #54526B;
+            }
+            button {
+                font-family: 'Ubuntu', sans-serif;
                 font-size: 18px;
                 margin-left: 480px;
                 width: 157px;
                 height: 34px;
+                border: none;
                 border-radius: 4px;
-            }
-            .civilbutton {
-                font-size: 18px;
-                margin-left: 50px;
-                width: 157px;
-                height: 34px;
-                border-radius: 4px;
-            }
-            .text {
-                text-align: center;
-                margin-top: 250px;
-                font-weight: bold;
+                background: #54526B;
+                text-decoration: none;
+                font-weight: 500;
+                color: #EAEAEA;
             }
         </style>
     </head>
     <body>
         <div>
-            <p class="text"> Masuk atau Daftar sebagai... </div></p>
+            <p class="text"> Lanjut sebagai... </div></p>
                 <div>
                     @auth
                         <a href="{{ url('/home') }}">Home</a>
                     @else
-                        <button class="adminbutton" type="button"> <a href="{{ route('login') }}">Pegawai</a> </button>
+                        <button class="adminbutton" type="button" onclick="window.location='{{ route('login') }}'">Administrator</button>
 
                         @if (Route::has('register'))
-                        <button class="civilbutton" type="button"><a href="{{ route('register') }}" class="ml-4 text-sm text-gray-700 underline">Masyarakat</a></button>
+                        <button class="civilbutton" type="button" onclick="window.location='{{ route('register') }}'">Masyarakat</button>
                         @endif
                     @endauth
                 </div>
         </div>
+        <div class="square"></div>
     </body>
 </html>
